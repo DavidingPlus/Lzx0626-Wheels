d91f93728aab3b0a95290727e2971f61b0bb0732
DavidingPlus <2065666169@qq.com>
2024-01-08 21:28:06 +0800

    update README.md, CMakeLists.txt and uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,9 @@
 # 1. 删除/usr/include/lzx0626目录
 sudo rm -rf /usr/include/lzx0626
 
+echo "Deleting header file successfully."
+
 # 2. 删除/lib64目录中的liblzx0626*.a文件
 sudo rm -rf /lib64/liblzx0626*.a
+
+echo "Deleting static library successfully."
