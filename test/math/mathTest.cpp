81159a14cbd8fc47a23a3828cb0b0e17ea9de31e
DavidingPlus <2065666169@qq.com>
2024-01-08 23:29:49 +0800

    update CMakeLists.txt

---
+++ test/math/mathTest.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include "four_rule_operation.h"
+
+/**
+ * @brief 加法测试
+ *
+ */
+TEST(mathTest, addTest) {
+}
+
+/**
+ * @brief 减法测试
+ *
+ */
+TEST(mathTest, subTest) {
+}
+
+/**
+ * @brief 乘法测试
+ *
+ */
+TEST(mathTest, mulTest) {
+}
+
+/**
+ * @brief 除法测试
+ *
+ */
+TEST(mathTest, divTest) {
+}
+/**
+ * @brief 除法失败测试
+ *
+ */
+TEST(mathTest, divFailTest) {
+}
