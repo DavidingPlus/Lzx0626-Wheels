816cd90cd68a4ca85bf0e32186740b93822472d1
DavidingPlus <2065666169@qq.com>
2024-01-09 15:11:49 +0800

    fix install.sh and uninstall.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,19 @@
 # 1.卸载旧版本
 # 删除/usr/include/lzx0626目录
 sudo rm -rf /usr/include/lzx0626
-
 echo "Delete header file successfully."
 
 # 删除/lib64目录中的liblzx0626*.a文件
 sudo rm -rf /lib64/liblzx0626*.a
-
 echo "Delete static library successfully."
 
 # 2.通过CMake工具构建编译
-if [ -d "build" ]; then
-    rm -rf build
-    echo "Build directory deleted, now creating a new one..."
+if [ ! -d "build" ]; then
+    mkdir build
 fi
 
-mkdir build
 cd build
-cmake ..
+cmake -Denable_snippet=OFF -Denable_gtest=OFF ..
 make
 
 # 3.安装
--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,8 @@
 
 # 1. 删除/usr/include/lzx0626目录
 sudo rm -rf /usr/include/lzx0626
-
 echo "Delete header file successfully."
 
 # 2. 删除/lib64目录中的liblzx0626*.a文件
 sudo rm -rf /lib64/liblzx0626*.a
-
 echo "Delete static library successfully."
