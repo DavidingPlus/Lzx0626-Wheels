dc7968447bdcc24696518799c23a18e24e72ec03
DavidingPlus <2065666169@qq.com>
2024-01-09 15:15:22 +0800

    delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-# 自动化测试脚本
-if [ ! -d "build" ]; then
-    mkdir build
-    echo "Build directory deleted, now creating a new one..."
-fi
-
-cd build
-cmake -Denable_snippet=ON -Denable_gtest=ON ..
-make -j6
