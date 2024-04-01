0097140b67a88f02cbf97596c5c2a824443055e9
DavidingPlus <2065666169@qq.com>
2024-04-01 15:35:43 +0800

    fix CMakeLists.txt

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-# 自动化测试脚本
 if [ ! -d "build" ]; then
     mkdir build
     echo "Build directory deleted, now creating a new one..."
 fi
 
 cd build
-cmake -DENABLE_GTEST=ON ..
+cmake -DENABLE_GTEST=OFF ..
 make -j6
+
+# TODO
+# make install
